511d2bc9d9674136d96fbeb267c43c0584a2efb4
Kendra Lyndon <kendralyndon@gmail.com>
2016-08-01 16:42:51 -0600

    add basic routes

---
+++ routes/tenders.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+var knex = require('../db/knex');
+
+router.get('/', function(req, res, next) {
+  res.send('hello');
+});
+
+module.exports = router;
