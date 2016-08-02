91570336fe4f50de8d82a04894db874d6182eaa0
Jessica Lee <JessLee@Jess-MacBook-Pro.local>
2016-08-02 12:12:23 -0600

    bidders route wip

--- routes/bids-sent.js
+++ routes/bids-sent.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('bidder/index', {
+    title: "When user is on bidder index page and clicks on an offer, user is directed to bidder view page"
+  });
 });
 
+router.get('/:id', function(req, res, next) {
+  res.render('bidder/show', {
+    title: "Meet Up"
+  })
+});
 module.exports = router;
