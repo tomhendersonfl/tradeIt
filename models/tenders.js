aed5f090a929e1685ced922453d01ebde5851262
Jim Watkins <james.watkins77@yahoo.com>
2016-08-01 19:45:22 -0600

    created tender show page

--- models/tenders.js
+++ models/tenders.js
@@ -3,16 +3,16 @@ module.exports = {
   create: function(tender) {
     return knex.raw(`insert into tenders values (DEFAULT, '${tender.name}', 'draft', '${tender.description}', '${tender.type}', ${tender.user_id}, NULL, NULL, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)`)
   },
-  table.increments();
-  table.string('name');
-  table.string('state');
-  table.text('description');
-  table.string('tender_type');
-  table.integer('user_id');
-  table.dateTime('published_at');
-  table.dateTime('accepted_at');
-  table.integer('nbr_views');
-  table.timestamps();
+  // table.increments();
+  // table.string('name');
+  // table.string('state');
+  // table.text('description');
+  // table.string('tender_type');
+  // table.integer('user_id');
+  // table.dateTime('published_at');
+  // table.dateTime('accepted_at');
+  // table.integer('nbr_views');
+  // table.timestamps();
   all: function() {
     return knex.raw(`select * from tenders order by name`)
   },
@@ -24,19 +24,20 @@ module.exports = {
   },
   destroy: function(id) {
     return knex.raw(`delete from tenders where id = ${id}`)
-  },
-  validateCreate: function(tender) {
-    var errors = []
-    if tender.name.trim().length === 0 {
-      errors.push("Name cannot be blank")
-    }
-    if tender.email_address.trim().length = 0 {
-      errors.push("Email address cannot be blank")
-    }
-    var re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-    if (!re.test(tender.email_address)) {
-      errors.push("Email address is not valid")
-    }
-    return errors
   }
+  // ,
+  // validateCreate: function(tender) {
+  //   var errors = []
+  //   if tender.name.trim().length === 0 {
+  //     errors.push("Name cannot be blank")
+  //   }
+  //   if tender.email_address.trim().length = 0 {
+  //     errors.push("Email address cannot be blank")
+  //   }
+  //   var re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+  //   if (!re.test(tender.email_address)) {
+  //     errors.push("Email address is not valid")
+  //   }
+  //   return errors
+  // }
 }
--- routes/tenders.js
+++ routes/tenders.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
+var Logic = require('../models/tenders')
 
 function Tenders(){
   return knex('tenders');
@@ -9,6 +10,12 @@ function Tenders(){
 router.get('/', function(req, res, next) {
   Tenders().select().then(function(tenders){
     res.render('tenders/index', { tenders:tenders, title: 'Trade It' });
+  })
+});
+
+router.get('/:id', function(req, res, next) {
+  Logic.find(req.params.id).then(function(tenders){
+    res.render('tenders/show', { tenders:tenders.rows[0], title: 'Trade It' });
   })
 });
 
