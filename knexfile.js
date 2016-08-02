601d82b7fec091014a4e70a505e395a78dccd7f4
Kendra Lyndon <kendralyndon@gmail.com>
2016-08-02 17:17:03 -0600

    change database url var

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.database_url + '?ssl=true'
+    connection: process.env.DATABASE_URL + '?ssl=true'
   }
 
 };
