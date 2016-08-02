dca00f08fa92a2bb2dd35f38ca55cc89aa68d198
Jessica Lee <JessLee@Jess-MacBook-Pro.local>
2016-08-02 15:51:47 -0600

    index page for bids, show page for each bid working

--- routes/bids-sent.js
+++ routes/bids-sent.js
@@ -1,24 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
-
-function Tenders(){
-  return knex('tenders');
-};
+var Tenders = require('../models/tenders')
 
 router.get('/', function(req, res, next) {
-  Tenders().select().then(function(tenders){
-    console.log(tenders[0]);
-    res.render('bidder/index', { tenders:tenders[0],
+  Tenders.all().then(function(tenders){
+    console.log(tenders.rows[0]);
+    res.render('bidder/index', { tenders:tenders.rows[0],
       message:'When user clicks accept offer on biddee show page, user is directed to meetup form'
-
     });
   })
 })
 
 router.get('/:id', function(req, res, next) {
+  Tenders.find(req.params.id).then(function(tenders){
   res.render('bidder/show', {
-    message:'When user clicks accept offer on biddee show page, user is directed to meetup form'
+    tenders: tenders.rows[0],
+    message:'Seeing a single bids page'
+    })
   })
 });
 
