40ca1746c5958179eea20fca2e3eb73fae372dc5
Kendra Lyndon <kendralyndon@gmail.com>
2016-08-02 16:18:42 -0600

    add basic bids-received page

--- routes/bids-received.js
+++ routes/bids-received.js
@@ -2,18 +2,8 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 
-function Tenders(){
-  return knex('tenders');
-};
-
 router.get('/', function(req, res, next) {
-
-  res.render('index', { title: 'Trade It' });
-  Tenders().select().then(function(tenders){
-    res.render('index', { tenders:tenders, title: 'Trade It' });
-  })
+  res.render('bids_received/index');
 })
-
-
 
 module.exports = router;
