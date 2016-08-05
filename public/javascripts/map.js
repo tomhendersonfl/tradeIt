4bc3ac68e667f832397a19f2c9cb170373e52723
Kendra Lyndon <kendralyndon@gmail.com>
2016-08-05 08:47:50 -0600

    m

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,5 +1,8 @@
 var map;
 
+var coords = JSON.parse($("#myCoords").val());
+console.log(coords);
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,10 @@ var FbInfo = require('../models/fbInfo');
 var Helpers = require('../models/helpers');
 var Mapkey = require('../models/mapkey');
 
+function UsersK(){
+  return knex('users')
+}
+
 router.get('/', function(req, res, next) {
 
   if(!req.cookies.userid){
@@ -19,10 +23,13 @@ router.get('/', function(req, res, next) {
 router.get('/dashboard', function(req, res, next){
   console.log(req.cookies.geolocation);
   Tenders.all().then(function(tenders){
-    if(tenders.rows.length > 20){
-      tenders.rows.splice(20)
-    }
-    res.render('dashboard', {tenders:tenders.rows, current_user_id:req.cookies.userid})
+    UsersK().select('position').then(function(locations){
+      console.log(locations);
+      if(tenders.rows.length > 20){
+        tenders.rows.splice(20)
+      }
+      res.render('dashboard', {tenders:tenders.rows, current_user_id:req.cookies.userid,locations:locations})
+    })
   })
 });
 
