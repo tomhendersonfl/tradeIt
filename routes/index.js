c77e5bb7ea15c760d9e0dc53a6dfc750af983765
Scott Wilkins <scottwilkins@Scotts-mac.local>
2016-08-05 06:33:38 -0600

    added show my tenders and made available tenders clickable

--- routes/index.js
+++ routes/index.js
@@ -11,51 +11,63 @@ var Mapkey = require('../models/mapkey');
 
 
 router.get('/', function(req, res, next) {
-  if(!req.cookies.userid){
-    res.cookie('userid', 100);
-  }
-  res.render('index', { title: 'Trade It', current_user_id:req.cookies.userid });
+    if (!req.cookies.userid) {
+        res.cookie('userid', 100);
+    }
+    res.render('index', {
+        title: 'Trade It',
+        current_user_id: req.cookies.userid
+    });
 })
 
-router.get('/dashboard', function(req, res, next){
-  Tenders.findByNotUserPublished(req.cookies.userid).then(function(tenders){
-    Bids.all().then(function(bids){
-    if(tenders.rows.length > 20){
-      tenders.rows.splice(20)
-    }
-    var bids_sent = [];
-    var bids_received = [];
-    for (var i = 0; i < bids.rows.length; i++) {
-      if (bids.rows[i].user_id === Number(req.cookies.userid)){
-        bids_sent.push(bids.rows[i]);
+router.get('/dashboard', function(req, res, next) {
+    Tenders.findByNotUserPublished(req.cookies.userid).then(function(tenders) {
+        Bids.all().then(function(bids) {
+            Tenders.findByUser(req.cookies.userid).then(function(myTenders) {
+                if (tenders.rows.length > 20) {
+                    tenders.rows.splice(20)
+                }
+                var bids_sent = [];
+                var bids_received = [];
+                for (var i = 0; i < bids.rows.length; i++) {
+                    if (bids.rows[i].user_id === Number(req.cookies.userid)) {
+                        bids_sent.push(bids.rows[i]);
 
-      } else {
-        bids_received.push(bids.rows[i]);
-      }
-    }
-    console.log("bids sent");
-    console.log(bids_sent);
-    console.log("bids received");
-    console.log(bids_received);
-    res.render('dashboard', {tenders:tenders.rows,
-            bids_sent:bids_sent,
-            bids_received:bids_received,
-            current_user_id:req.cookies.userid
-          })
+                    } else {
+                        bids_received.push(bids.rows[i]);
+                    }
+                }
+                res.render('dashboard', {
+                    tenders: tenders.rows,
+                    bids_sent: bids_sent,
+                    bids_received: bids_received,
+                    current_user_id: req.cookies.userid,
+                    myTenders: myTenders.rows
+                })
+            })
+
         })
-      })
+    })
 });
 
 router.get('/FAQ', function(req, res, next) {
-  res.render('FAQ', { title: 'Trade It', current_user_id:req.cookies.userid });
+    res.render('FAQ', {
+        title: 'Trade It',
+        current_user_id: req.cookies.userid
+    });
 })
 
-router.get('/contact', function(req, res, next){
-  res.render('contact', {current_user_id:req.cookies.userid});
+router.get('/contact', function(req, res, next) {
+    res.render('contact', {
+        current_user_id: req.cookies.userid
+    });
 })
 
 router.get('/about_us', function(req, res, next) {
-  res.render('about_us', { title: 'Trade It', current_user_id:req.cookies.userid });
+    res.render('about_us', {
+        title: 'Trade It',
+        current_user_id: req.cookies.userid
+    });
 });
 
 module.exports = router;
