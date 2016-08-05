14cdf2884077bb0bba166857625c60a420a134ca
Kendra Lyndon <kendralyndon@gmail.com>
2016-08-05 14:58:39 -0600

    goddammit

--- seeds/sample_questions.js
+++ seeds/sample_questions.js
@@ -6,15 +6,15 @@ exports.seed = function(knex, Promise) {
 
         knex('questions').insert({id: 1002, tender_id: 1008, question: 'How long is the willy warmer?', answer: 'Long enough for most.', created_at: '2016-04-01', updated_at: '2016-04-02'}),
 
-        knex('questions').insert({id: 1003, tender_id: 1009, question: 'How do we know it is the real planet Kolob?',, created_at: '2016-04-01', updated_at: '2016-04-02'}),
+        knex('questions').insert({id: 1003, tender_id: 1009, question: 'How do we know it is the real planet Kolob?', created_at: '2016-04-01', updated_at: '2016-04-02'}),
 
-        knex('questions').insert({id: 1004, tender_id: 1004, question: 'How big are its feet?',, created_at: '2016-04-01', updated_at: '2016-04-02'}),
+        knex('questions').insert({id: 1004, tender_id: 1004, question: 'How big are its feet?', created_at: '2016-04-01', updated_at: '2016-04-02'}),
 
-        knex('questions').insert({id: 1005, tender_id: 1004, question: 'How tall is the Bigfoot',, created_at: '2016-04-01', updated_at: '2016-04-02'}),
+        knex('questions').insert({id: 1005, tender_id: 1004, question: 'How tall is the Bigfoot', created_at: '2016-04-01', updated_at: '2016-04-02'}),
 
-        knex('questions').insert({id: 1006, tender_id: 1001, question: 'How do you know what the Virgin Mary looked like?',, created_at: '2016-04-01', updated_at: '2016-04-02'}),
+        knex('questions').insert({id: 1006, tender_id: 1001, question: 'How do you know what the Virgin Mary looked like?', created_at: '2016-04-01', updated_at: '2016-04-02'}),
 
-        knex('questions').insert({id: 1007, tender_id: 1001, question: 'What kind of cheese is it?',, created_at: '2016-04-01', updated_at: '2016-04-02'}),
+        knex('questions').insert({id: 1007, tender_id: 1001, question: 'What kind of cheese is it?', created_at: '2016-04-01', updated_at: '2016-04-02'}),
 
       ]);
     });
